cf6439abc3a09446ffc3fc59903582dfbef126a2
pulicatlabs <sriharikotamasthan@gmail.com>
2026-08-09 03:17:32 +0530

    Add Spring Boot application entry point

---
+++ backend/src/main/java/com/nnit/college360/College360Application.java
@@ -0,0 +1,11 @@
+package com.nnit.college360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class College360Application {
+    public static void main(String[] args) {
+        SpringApplication.run(College360Application.class, args);
+    }
+}
