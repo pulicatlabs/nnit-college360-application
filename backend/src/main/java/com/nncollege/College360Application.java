9c8e7a1e0ed6eeff86c19f6a172eb896d62b42a1
pulicatlabs <sriharikotamasthan@gmail.com>
2026-08-09 03:07:18 +0530

    Add Spring Boot application

---
+++ backend/src/main/java/com/nncollege/College360Application.java
@@ -0,0 +1,7 @@
+package com.nncollege;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class College360Application {
+ public static void main(String[] args){SpringApplication.run(College360Application.class,args);}
+}
